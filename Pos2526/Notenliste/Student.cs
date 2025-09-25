34ccc9b1bef4944e5de06a20ab8f133133bf8912
iSketchup <kalulu@gmx.at>
2025-09-25 10:58:49 +0200

    Refactor Student class and update StudentCol
    Changed Student ID type from uint to int and added constructors with validation. Updated StudentCol to be a public class and initialized a students list. Removed unused usings and improved code structure.

--- Pos2526/Notenliste/Student.cs
+++ Pos2526/Notenliste/Student.cs
@@ -2,9 +2,9 @@
 {
     public class Student
     {
-        private uint id;
+        private int id;
 
-        public uint ID
+        public int ID
         {
 
             get
@@ -46,6 +46,22 @@
             }
         }
 
+        // TODO: Add Grae Collection
 
+
+    public Student() { }
+        /// <summary>
+        /// Erstellt ein neues Sudent Obj mit dem übergebenen WErten
+        /// </summary>
+        /// <param name="id">Id des Schülers. Darf nicht negativ sien. </param>
+        /// <param name="firstName">Vorname. Darf nicht leer sein.</param>
+        /// <param name="lastName">Nachname. darf nicht leer sein</param>
+        /// <exception cref="ArgumentOutOfRangeException">Id darf nicht negativ sein. Vor und nachname dürfen nicht leer sein</exception>
+        public Student(int id, string firstName, string lastName)
+        {
+            this.ID = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
     }
 }
--- Pos2526/Notenliste/StudentCol.cs
+++ Pos2526/Notenliste/StudentCol.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Notenliste
+﻿namespace Notenliste
 {
-    class StudentCol
+    public class StudentCol
     {
+        private List<Student> students = new List<Student>();
+    
+
     }
 }
