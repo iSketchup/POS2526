f94583cc3ba7f9e14d0775c08b36043967e67f62
iSketchup <kalulu@gmx.at>
2025-11-06 12:28:18 +0100

    sigma

---
+++ Pos2526/Inheritance_Train/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Inheritance_Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Train train = new()
+            {
+                Name = "train"
+
+            };
+
+            Steamlocomotiv steamlocomotiv = new()
+            {
+                Name = "Thomas"
+            };
+
+
+            List<Train> trainList = new();
+
+            trainList.Add(train);
+            trainList.Add(steamlocomotiv); // kann dank polymorphysm auch geaddet werden
+
+            foreach (Train t in trainList) {
+                t.Horn();
+            }
+
+        }
+    }
+
+    public class Train
+    {
+        public double Speed { get; set; }
+
+        public string Name {  get; set; }
+
+        public virtual void Horn() // virtual erlaubt overrides von kinderklassen
+        { 
+            Console.WriteLine("tut tut tut sahur");
+        }
+    }
+    public class Steamlocomotiv : Train
+    {
+        public int Coal { get; set; }
+
+       
+        public override void Horn() // override überscheibt das verhalten der methode 
+        {
+            Console.WriteLine("cho cho");
+        }
+    
+    }
+
+}
