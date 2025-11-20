dbba75dfca87b753ed58ed94986dd9954d963a23
iSketchup <kalulu@gmx.at>
2025-11-20 14:12:28 +0100

    cook

---
+++ Pos2526/AbstractClassCook/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace AbstractClassCook
+{
+
+
+
+    public abstract class Product
+    {
+        public string name { get; protected set; }
+        public double price { get; protected set; }
+        protected string PRID { get; set; }
+
+        protected Product(string n, double p, string id)
+        {
+            name = n;
+            price = p;
+            PRID = id;
+        }
+    }
+    public class Food : Product
+    {
+        public DateOnly ExpireDate { get; protected set; }
+
+        public Food(string n, double p, string id, DateOnly exd) : base(n, p, id)
+        {
+            ExpireDate = exd;
+
+        }
+    }
+
+    public class Frigeware : Food
+    {
+        public double MinTemp { get; protected set; }
+        public double MaxTemp { get; protected set; }
+        public Frigeware(string n, double p, string id, DateOnly exp, double min, double max) : base(n, p, id, exp)
+        {
+            MinTemp = min;
+            MaxTemp = max;
+        }
+    }
+
+    public class NonFood : Product
+    {
+
+        public NonFood(string n, double p, string id) : base(n, p, id)
+        {
+        }
+    }
+
+    public class Clothing : NonFood
+    {
+        public string size { get; protected set; }
+        public DateOnly ProductionDate { get; protected set; }
+        public Clothing(string n, double p, string id, DateOnly pd, string size) : base(n, p, id)
+        {
+            ProductionDate = pd;
+            this.size = size;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                DateOnly date = new DateOnly();
+                Clothing shirt = new("Cooles shirt", 14.99, "0", date, "XL");
+                Clothing hat = new("Hat", 4.99, "1", date, "40");
+
+
+                Frigeware milk = new("milk", 2.99, "23", date, 4, -20);
+                Frigeware cheese = new("Gauda", 4.55, "45", date, 4, -10);
+
+                List<Product> products = new List<Product>();
+                products.Add(shirt);
+                products.Add(hat);
+                products.Add(cheese);
+                products.Add(milk);
+
+                foreach (Product product in products)
+                {
+                    Console.WriteLine(product.ToString());
+                }
+
+
+
+            }
+        }
+    }
+}
---
+++ Pos2526/InterfacecRPG/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Drawing;
+using System.Numerics;
+
+namespace InterfacecRPG
+{
+    public interface IDamagable
+    {
+        public void TakeDamage(int damage);
+    }
+
+    public interface IMoveable
+    {
+        public void move(Vector2 vec);
+    }
+
+
+    public abstract class Character : IMoveable
+    {
+        protected PointF pos = new(0,0);
+        public int Hp { get; set; } = 100;
+        public string Name { get; set; } = "";
+
+        public Character(int hp, string name)
+        {
+            Hp = hp;
+            Name = name;
+        }
+
+        public void move(Vector2 vec)
+        {
+            pos = new PointF(pos.X + vec.X, pos.Y + vec.Y);
+        }
+    }
+
+
+    public class Mage : Character, IDamagable
+    {
+        public Mage(int hp, string name) : base(hp, name)
+        {
+
+        }
+
+
+        public void TakeDamage(int damage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
